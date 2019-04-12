3ae6cb096103adb96e4381e9715fd95f5d2a5324
alper ozcelik <alp_ozelik@hotmail.com>
2019-04-12 16:33:18 +0300

    init

---
+++ StarPyramid/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace StarPyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(
+                new[] { Enumerable.Range(1, 10) }
+                .Select(p => p.Where(p1 => p1 % 2 != 0)
+                              .Select(item =>
+                                    new string(' ', (p.Count() - item) / 2) +
+                                    new string('*', item) +
+                                     Environment.NewLine)
+                )
+                .FirstOrDefault()
+                .Aggregate("", (agg, item) => agg + item)
+            );
+
+            Console.ReadLine();
+        }
+    }
+}
